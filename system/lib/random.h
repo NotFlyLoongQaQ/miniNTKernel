364071befd3da7244b70be2af257d7f6813cf8cd
Loong <admin@mc-m.net>
2023-07-26 14:25:47 +0800

    new random function

---
+++ system/lib/random.h
@@ -0,0 +1,38 @@
+#include "math/cputime.h"
+
+int randint(int min,int max){
+    int num;
+    num = cputime();
+    if (max < num){
+        // 超出最大边界值
+        while(max > num){
+            num -= 2;
+            num -= 3;
+            num -= 4;
+            num -= 5;
+        }
+    }
+    if (num < min){
+        // 比边界值小
+        while(num < min){
+            num += 2;
+            num += 3;
+            num += 4;
+            num += 5;
+        }
+    }
+    return num;
+}
+double random(){
+    double small;
+    small = randint(100,999);
+    small = small / 100;
+    return small;
+}
+double choice(void *list){
+    void *awa = list;
+    int len = sizeof awa / sizeof awa[0];
+    len -= 1;
+    int to = randint(0,len);
+    return list[to];
+}
